269e6cd8156e03feb2563f82ce4b05756b932f56
Adam Barton <adam.barton333@gmail.com>
2018-10-12 01:57:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,6 +8,14 @@ class App < Sinatra::Base
 
   post '/team' do
     params.to_s
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+   @c = params[:c]
+    
   end
 
   # get '/team' do
